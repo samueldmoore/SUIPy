efa4cfb34f8b2e23370a9626b0767c963750548c
samueldmoore <samueld.moore@outlook.com>
2022-04-25 16:57:42 -0400

    Make testing file

---
+++ test_suipy_framework.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+""" Tests for the SUIPy framework implemented using a variety of strategies.
+
+First, the intermediate-level classes such as the GUI are to be tested using a
+logic criteria like correlated active clause coverage.
+
+"""
